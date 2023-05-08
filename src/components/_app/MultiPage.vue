0fabbfeb8ef8182c1ee129464fea588f59aad186
Maleesha Gimshan <maleeshagimshan74@gmail.com>
2023-05-09 00:08:39 +0530

    added multipage component - tested

---
+++ src/components/_app/MultiPage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="">
+        <slot :currentPage="currentPage" :next="next" :prev="prev" :jump="jump"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            currentPage: 1,
+        }
+    },
+    props: {
+        pages: {
+            type: [Number],
+        },
+        startAt: {
+            type: [Number],
+            default: 1,
+        },
+    },
+    methods: {
+        next() {
+            if (this.currentPage == this.pages) {
+                throw new Error(
+                    `Error in MultiPage.vue component, trying to access an undefined page number`
+                )
+            }
+            this.currentPage++
+            console.log(this.currentPage)
+        },
+        prev() {
+            if (this.currentPage == 1) {
+                throw new Error(
+                    `Error in MultiPage.vue component, no previous page on page 01 but tried to access page number`
+                )
+            }
+            this.currentPage--
+            console.log(this.currentPage)
+        },
+        jump(pageNo = null) {
+            if (pageNo > this.currentPage && pageNo < this.pages) {
+                this.currentPage = pageNo
+            }
+            if (pageNo < this.currentPage && pageNo > 0) {
+                this.currentPage = pageNo
+            } else {
+                throw new Error(
+                    `Error in MultiPage.vue component, trying to access an invalid page number - ${pageNo}`
+                )
+            }
+        },
+    },
+    async mounted() { },
+    beforeMount() { },
+}
+</script>
+
+<style></style>
