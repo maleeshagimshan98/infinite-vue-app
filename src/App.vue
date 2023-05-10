4f422d1e872f06d26956eb3f058d1a24ac26a360
Maleesha Gimshan <maleeshagimshan74@gmail.com>
2023-05-10 21:28:50 +0530

    Created a separate component for layouting

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,37 @@
 <template>
-  <div class="">
-    <!-- layout components-->
-    <!-- reccomended to use components in _app folder as wrapper components for following components-->
-    <app-bar></app-bar>
-    <side-menu></side-menu>
-    <!-- ---------------------->
-
-    <router-view v-slot="{ Component }">
-      <!-- App-wide default transitions -->
-      <transition>
-        <Component :is="Component"></Component>
-      </transition>
-    </router-view>
-
+  <div class="">    
+    <default-layout>
+      <router-view v-slot="{ Component }">
+        <!-- App-wide default transitions -->
+        <transition>
+          <Component :is="Component"/>
+        </transition>
+      </router-view>
+    </default-layout>
   </div>
 </template>
 
 <script>
+import DefaultLayout from "./layouts/DefaultLayout.vue"
 import AppBar from "./components/AppBar.vue"
 import SideMenu from "./components/SideMenu.vue"
 
 export default {
-  data () {
+  data() {
     return {
       //... add vue meta data for setting title on the browser tab
     }
   },
-  props : {},
-  methods : {},
-  components : {
-    AppBar, SideMenu
+  props: {},
+  methods: {},
+  components: {
+    AppBar,
+    SideMenu,
+    DefaultLayout,
   },
-  async mounted () {},
-
+  async mounted() { },
+  beforeMount () {},
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <!-- layout components-->
+        <app-bar />
+        <side-menu />
+        <slot>
+            <!-- main content -->
+        </slot>
+        <slot name="footer">
+            <!-- footer content -->
+        </slot>
+    </div>
+</template>
+
+<script>
+import AppBar from '../components/AppBar.vue';
+import SideMenu from '../components/SideMenu.vue';
+
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+    props: {},
+    components: {
+        AppBar, SideMenu
+    },
+    async mounted() {
+
+    }
+}
+</script>
