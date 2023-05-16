b351cee0eb27526f1e093952f8918f248895a98c
Maleesha Gimshan <maleeshagimshan74@gmail.com>
2023-05-17 01:32:37 +0530

    updated tests for the vuex store

--- test/store.test.js
+++ test/store.test.js
@@ -1,87 +1,141 @@
-import { createStore } from 'vuex'
-import initStore from '../src/store/store.js'
+import { createStore } from "vuex"
+import initStore from "../src/store/store.js"
 
-describe('Vuex Store Actions', () => {  
+describe("Vuex Store Actions", () => {
   let store
 
   beforeEach(() => {
     store = createStore(initStore())
   })
 
-  test('setAppBarTitle action', () => {
-    const title = 'New Title'
-    store.dispatch('setAppBarTitle', title)
+  test("setAppBarTitle action", () => {
+    const title = "New Title"
+    store.dispatch("setAppBarTitle", title)
     expect(store.state._meta.appBarTitle).toBe(title)
   })
 
-  test('setPreviousPage action', () => {
-    const previousPage = '/previous-page'
-    store.dispatch('setPreviousPage', previousPage)
+  test("setCookiesAllowedState action", () => {
+    const isCookiesAllowed = false
+    store.dispatch("setCookiesAllowedState", isCookiesAllowed)
+    expect(store.state._meta.isCookiesAllowed).toBe(isCookiesAllowed)
+  })
+
+  test("setPreviousPage action", () => {
+    const previousPage = "/previous-page"
+    store.dispatch("setPreviousPage", previousPage)
     expect(store.state._meta.previousPage).toBe(previousPage)
   })
 
-  test('setOverlayStatus action', () => {
+  test("setOverlayStatus action", () => {
     const isOverlay = true
-    store.dispatch('setOverlayStatus', isOverlay)
+    store.dispatch("setOverlayStatus", isOverlay)
     expect(store.state._meta.isOverlayPresent).toBe(isOverlay)
   })
 
-  test('setNavigationGuard action', () => {
+  test("setPreventBackNav action", () => {
     const value = true
-    store.dispatch('setNavigationGuard', value)
-    expect(store.state._meta.navigationGuard).toBe(value)
+    store.dispatch("setPreventBackNav", value)
+    expect(store.state._meta.preventBackNav).toBe(value)
   })
 
-  test('addErrorMessage action', () => {
-    const error = 'An error occurred'
-    store.dispatch('addErrorMessage', error)
+  test("addErrorMessage action", () => {
+    const error = "An error occurred"
+    store.dispatch("addErrorMessage", error)
     expect(store.state._meta.isError).toBe(true)
     expect(store.state._meta.errors).toContain(error)
   })
 
-  test('removeErrorMessage action', () => {
-    const error1 = 'Error 1'
-    const error2 = 'Error 2'
-    store.dispatch('addErrorMessage', error1)
-    store.dispatch('addErrorMessage', error2)
-    store.dispatch('removeErrorMessage')
+  test("removeErrorMessage action", () => {
+    const error1 = "Error 1"
+    const error2 = "Error 2"
+    store.dispatch("addErrorMessage", error1)
+    store.dispatch("addErrorMessage", error2)
+    store.dispatch("removeErrorMessage")
     expect(store.state._meta.errors).toEqual([error1])
-    store.dispatch('removeErrorMessage')
+    store.dispatch("removeErrorMessage")
     expect(store.state._meta.errors).toEqual([])
     expect(store.state._meta.isError).toBe(false)
   })
 
-  test('api action', () => {
-    // Test the scenario when apiClient is defined
+  test("api action", () => {
+    // Mock the apiClient object
     const apiClient = {
       // Mock implementation of the apiClient methods
-      analytics: jest.fn()
+      analytics: jest.fn(),
     }
     store.state.apiClient = apiClient
 
-    store.dispatch('api', 'payload')
+    store.dispatch("api", "payload")
     expect(store.state.isDataLoading).toBe(true)
-    // Add more assertions for the expected behavior when apiClient is defined
+    // Add more assertions for the expected behavior
 
     // Test the scenario when apiClient is not defined
     store.state.apiClient = null
 
     expect(() => {
-      store.dispatch('api', 'payload')
-    }).toThrowError('Api client is not defined')
+      store.dispatch("api", "payload")
+    }).toThrowError("Api client is not defined")
     expect(store.state.isDataLoading).toBe(false)
-    // Add more assertions for the expected behavior when apiClient is not defined
+    // Add more assertions for the expected behavior
   })
 
-  test('sendPageAnalyticsData action', () => {
-    // Mock the apiClient.analytics() method
+  test("sendPageViewAnalytics action", () => {
+    // Mock the pageViewAnalyticsClosure function
+    const pageViewAnalyticsClosure = jest.fn()
+    store.state.pageViewAnalyticsClosure = pageViewAnalyticsClosure
+
+    const data = "analytics data"
+    store.dispatch("sendPageViewAnalytics", data)
+    expect(pageViewAnalyticsClosure).toHaveBeenCalledWith(data)
+    // Add more assertions for the expected behavior
+  })
+
+  test("sendErrorLogs action", () => {
+    // Mock the errorLoggingClosure function
+    const errorLoggingClosure = jest.fn()
+    store.state.errorLoggingClosure = errorLoggingClosure
+
+    const data = "error data"
+    store.dispatch("sendErrorLogs", data)
+    expect(errorLoggingClosure).toHaveBeenCalledWith(data)
+    // Add more assertions for the expected behavior
+  })
+
+  test("sendAnalyticsData action", () => {
+    // Mock the apiClient object
     const apiClient = {
-      analytics: jest.fn()
+      // Mock implementation of the apiClient methods
+      analytics: jest.fn(),
     }
     store.state.apiClient = apiClient
-
-    store.dispatch('sendPageAnalyticsData', 'data')
+    const data = "analytics data"
+    store.dispatch("sendAnalyticsData", data)
     expect(apiClient.analytics).toHaveBeenCalled()
-    // Add more assertions for the expected behavior
+    // Add more assertions for the expected behavior when apiClient is defined
+
+    // Test the scenario when apiClient is not defined
+    store.state.apiClient = null
+
+    // Add assertions for the expected behavior when apiClient is not defined
+  })
+})
+
+describe("Vuex Store Getters", () => {
+  let store
+
+  beforeEach(() => {
+    store = createStore(initStore())
+  })
+
+  test("hasNavigationGuard getter", () => {
+    expect(store.getters.hasNavigationGuard).toBe(false)
+    store.state._meta.preventBackNav = true
+    expect(store.getters.hasNavigationGuard).toBe(true)
+  })
+
+  test("previousPage getter", () => {
+    const previousPage = "/previous-page"
+    store.state._meta.previousPage = previousPage
+    expect(store.getters.previousPage).toBe(previousPage)
   })
 })
