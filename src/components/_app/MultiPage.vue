bee701b5ce51b0370d5257ce4fb1d2528e90f2e2
maleeshagimshan98 <47087449+maleeshagimshan98@users.noreply.github.com>
2023-10-11 14:42:46 +0530

    Removed unnecessary div from MultiPage.vue

--- src/components/_app/MultiPage.vue
+++ src/components/_app/MultiPage.vue
@@ -1,61 +1,58 @@
 <template>
-    <!-- recommended to use components in the _app folder as wrapper components -->
-    <div class="">
-        <slot :currentPage="currentPage" :next="next" :prev="prev" :jump="jump"></slot>
-    </div>
+  <!-- recommended to use components in the _app folder as wrapper components -->
+  <slot :currentPage="currentPage" :next="next" :prev="prev" :jump="jump"></slot>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            currentPage: 1,
-        }
+  data() {
+    return {
+      currentPage: 1,
+    }
+  },
+  props: {
+    pages: {
+      type: [Number],
     },
-    props: {
-        pages: {
-            type: [Number],
-        },
-        startAt: {
-            type: [Number],
-            default: 1,
-        },
+    startAt: {
+      type: [Number],
+      default: 1,
     },
-    methods: {
-        next() {
-            if (this.currentPage == this.pages) {
-                throw new Error(
-                    `Error in MultiPage.vue component, trying to access an undefined page number`
-                )
-            }
-            this.currentPage++
-            console.log(this.currentPage) //... testing
-        },
-        prev() {
-            if (this.currentPage == 1) {
-                throw new Error(
-                    `Error in MultiPage.vue component, no previous page on page 01 but tried to access page number`
-                )
-            }
-            this.currentPage--
-            console.log(this.currentPage) //... testing
-        },
-        jump(pageNo = null) {
-            if (pageNo === this.currentPage) {
-                return
-            }
-            if (pageNo > 0 && pageNo < this.pages) {
-                this.currentPage = pageNo
-            }
-            else {
-                throw new Error(
-                    `Error in MultiPage.vue component, trying to access an invalid page number - ${pageNo}`
-                )
-            }
-        },
+  },
+  methods: {
+    next() {
+      if (this.currentPage == this.pages) {
+        throw new Error(
+          `Error in MultiPage.vue component, trying to access an undefined page number`
+        )
+      }
+      this.currentPage++
+      console.log(this.currentPage) //... testing
     },
-    async mounted() { },
-    beforeMount() { },
+    prev() {
+      if (this.currentPage == 1) {
+        throw new Error(
+          `Error in MultiPage.vue component, no previous page on page 01 but tried to access page number`
+        )
+      }
+      this.currentPage--
+      console.log(this.currentPage) //... testing
+    },
+    jump(pageNo = null) {
+      if (pageNo === this.currentPage) {
+        return
+      }
+      if (pageNo > 0 && pageNo < this.pages) {
+        this.currentPage = pageNo
+      } else {
+        throw new Error(
+          `Error in MultiPage.vue component, trying to access an invalid page number - ${pageNo}`
+        )
+      }
+    },
+  },
+  async mounted() {},
+  beforeMount() {},
 }
 </script>
 
