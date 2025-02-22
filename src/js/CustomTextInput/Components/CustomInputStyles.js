f03ac0a20b93e9a3033ff4f2d71eaa8977d36778
Maleesha Gimshan <maleeshagimshan74@gmail.comn>
2025-02-23 02:40:49 +0530

    passed the required parameters for creation of CustomInputStyles to use as app-wide styles for CustomTextInput components

--- src/js/CustomTextInput/Components/CustomInputStyles.js
+++ src/js/CustomTextInput/Components/CustomInputStyles.js
@@ -1,3 +1,26 @@
-import {CustomInputStyles} from "vue-custom-text-input"
+import { CustomInputStyles } from "vue-custom-text-input"
 
-export default {styles : new CustomInputStyles({})}
+export default new CustomInputStyles({
+  //...  TODO - add css class names for each state of each section of element
+  input: {
+    base: [],
+    primary: [],
+    focused: [],
+    error: [],
+    success: [],
+  },
+  label: {
+    base: [],
+    primary: [],
+    focused: [],
+    error: [],
+    success: [],
+  },
+  message: {
+    base: [],
+    primary: [],
+    focused: [],
+    error: [],
+    success: [],
+  },
+})
