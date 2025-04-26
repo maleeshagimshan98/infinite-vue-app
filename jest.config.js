a3d27df7921018638f0a792ea21240345792b67e
Maleesha Gimshan <47087449+maleeshagimshan98@users.noreply.github.com>
2025-04-27 01:17:07 +0530

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'vue'],
   transform: {
+    '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.vue$': '@vue/vue3-jest', // Transform Vue files
     '^.+\\.[tj]sx?$': 'babel-jest', // Transform JS/TS files
   },
@@ -8,7 +9,6 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1', // Map '@' alias to 'src'
     '^~bootstrap$': '<rootDir>/node_modules/bootstrap', // Map bootstrap alias
   },
-  testMatch: ['<rootDir>/test/**/*.test.[jt]s'],
+  testMatch: ['<rootDir>/**/*.spec.[jt]s'],
   setupFilesAfterEnv: ['<rootDir>/test/setup.js'],
-  testEnvironment: 'jsdom', // Required for Vue testing
 };
